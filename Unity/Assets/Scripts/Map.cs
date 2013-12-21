fb58d589c3db849f248cf43935a57fc7c888f7cf
Andrea Distler <andrea.distler@gmail.com>
2013-12-21 23:22:30 +0100

    added unity project

---
+++ Unity/Assets/Scripts/Map.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour
+{
+	[SerializeField]
+	private Tile tilePrefab;
+
+	[SerializeField]
+	private float tileSize = 1;
+
+	[SerializeField]
+	private int mapSize = 10;
+
+	Tile[,] tiles;
+
+	// Use this for initialization
+	void Start()
+	{
+		Generate(mapSize);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	void Generate(int size)
+	{
+		tiles = new Tile[size, size];
+
+		Vector3 tPos = Vector3.zero;
+		tPos.x = transform.position.x - (size - 1) * 0.5f * tileSize;
+		tPos.z = transform.position.z - (size - 1) * 0.5f * tileSize;
+
+
+		for (int x = 0; x < size; x++)
+		{
+			for (int y = 0; y < size; y++)
+			{
+				tiles[x, y] = Instantiate(tilePrefab, tPos, Quaternion.Euler(Vector3.right * 90)) as Tile;
+				tiles[x, y].transform.parent = transform;
+				tiles[x, y].name += " " + x + " " + y;
+
+				tPos.z += tileSize;
+			}
+
+			tPos.z = transform.position.z - (size - 1) * 0.5f * tileSize;
+			tPos.x += tileSize;
+		}
+	}
+
+	void Pollute(int x, int y)
+	{
+		tiles[x, y].Pollute();
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.DrawWireCube(transform.position, Vector3.right * mapSize * tileSize + Vector3.forward * mapSize * tileSize);
+	}
+}
---
+++ Unity/Assets/Scripts/Tile.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour
+{
+	public enum Type { Earth };
+
+	public float Pollution { get; set; } // between 0 and 1, 0 - not polluted
+
+	[SerializeField]
+	private Type type = Type.Earth;
+
+	// in percent per second
+	[SerializeField]
+	public float pollutionGrowthRate = 1.0f / 120;
+
+	// the current pollution change rate
+	private float pollutionChangeRate = 0;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		Pollution = Pollution + pollutionChangeRate * Time.deltaTime;
+		
+		renderer.material.color = new Color(1 - Pollution, 1 - Pollution, 1 - Pollution);
+	}
+
+	public void Pollute()
+	{
+		pollutionChangeRate = pollutionGrowthRate;
+	}
+}
